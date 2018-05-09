9fb65f85798f01026fbac8a8517afe8d6cbf2c80
tan <1311230692@qq.com>
2018-05-09 14:55:58 +0800

    信息管理系统 1.2018-05-08 13:46

--- src/main/java/ssm/controller/Login.java
+++ src/main/java/ssm/controller/Login.java
@@ -2,6 +2,8 @@ package ssm.controller;
 
 public class Login {
     public static void printArr(int[] arr) {
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println();
+        }
     }
 }
